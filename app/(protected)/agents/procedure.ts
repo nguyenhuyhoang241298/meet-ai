22651a16a6a1b814cdbf600ff1bad2879177ce63
nguyenhuyhoang <nguyenhuyhoang241298@gmail.com>
2025-06-26 16:06:33 +0700

    feat(agent): protected procedure, create agent form

--- app/(protected)/agents/procedure.ts
+++ app/(protected)/agents/procedure.ts
@@ -1,9 +1,34 @@
 import db from '@/db'
 import { agents } from '@/db/schema'
-import { baseProcedure, createTRPCRouter } from '@/trpc/init'
+import { createTRPCRouter, protectedProcedure } from '@/trpc/init'
+import { eq } from 'drizzle-orm'
+import { z } from 'zod'
+import { agentInsertSchema } from './schema'
 
 export const agentsRouter = createTRPCRouter({
-  getMany: baseProcedure.query(async () => {
-    return db.select().from(agents)
+  getOne: protectedProcedure
+    .input(z.object({ id: z.string() }))
+    .query(async ({ input }) => {
+      const [agent] = await db
+        .select()
+        .from(agents)
+        .where(eq(agents.id, input.id))
+
+      return agent
+    }),
+
+  getMany: protectedProcedure.query(async () => {
+    return await db.select().from(agents)
   }),
+
+  create: protectedProcedure
+    .input(agentInsertSchema)
+    .mutation(async ({ input, ctx }) => {
+      const [newAgent] = await db
+        .insert(agents)
+        .values({ ...input, userId: ctx.auth.user.id })
+        .returning()
+
+      return newAgent
+    }),
 })
