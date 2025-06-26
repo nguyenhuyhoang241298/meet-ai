4605260993e8701736a67b497d7129241476ca87
nguyenhuyhoang <nguyenhuyhoang241298@gmail.com>
2025-06-26 17:46:56 +0700

    feat(agent-table): create agent table, filter, edit agent

--- app/(protected)/agents/page.tsx
+++ app/(protected)/agents/page.tsx
@@ -2,12 +2,13 @@
 
 import { useTRPC } from '@/trpc/client'
 import { useSuspenseQuery } from '@tanstack/react-query'
+import { DataTable } from './table/table'
 
 const Page = () => {
   const trpc = useTRPC()
   const { data } = useSuspenseQuery(trpc.agents.getMany.queryOptions())
 
-  return <div>{JSON.stringify(data, null, 2)}</div>
+  return <DataTable data={data} />
 }
 
 export default Page
--- app/(protected)/agents/schema.ts
+++ app/(protected)/agents/schema.ts
@@ -4,3 +4,9 @@ export const agentInsertSchema = z.object({
   name: z.string().min(1, 'Name is required'),
   instructions: z.string().min(1, 'Instructions is required'),
 })
+
+export const agentUpdateSchema = z.object({
+  name: z.string().min(1, 'Name is required'),
+  instructions: z.string().min(1, 'Instructions is required'),
+  id: z.string().min(1, 'ID is required'),
+})
