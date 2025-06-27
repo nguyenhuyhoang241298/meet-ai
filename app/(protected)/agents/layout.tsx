e57961cd1795c483b85658902ae8a743753b0c76
nguyenhuyhoang <nguyenhuyhoang241298@gmail.com>
2025-06-27 15:02:16 +0700

    feat(agent-table): debounce for search

--- app/(protected)/agents/layout.tsx
+++ app/(protected)/agents/layout.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import ListHeader from './list-header'
+import SearchAgents from './table/header'
 
 const Layout = async ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="p-6 space-y-6">
       <ListHeader />
+      <SearchAgents />
       {children}
     </div>
   )
